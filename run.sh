97b1c29fa0c451fd0ca7c4c814a2c488bda37b90
muskgift <81701458+muskgift@users.noreply.github.com>
2021-06-02 23:20:54 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-sudo git clone https://github.com/smartholdem/smartholdem-rpc.git
+sudo git clone https://github.com/smartholdem/smartholdem-rpc
 cd smartholdem-rpc
 npm install
 forever start server.js
 
-sudo apt install python-pip
+sudo apt install python3-pip
 cd withdrawal-coins
-pip install -r requirements.txt
-sudo python withdrawal_coins.py
+pip3 install -r requirements.txt
+sudo python3 withdrawal_coins.py
 forever stop server.js
